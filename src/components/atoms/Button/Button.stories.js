e2bf907a0739032ec6b14804797a21f26212cd64
Maciej <hydroggen@o2.pl>
2022-02-08 13:34:46 +0100

    implement storybook

---
+++ src/components/atoms/Button/Button.stories.js
@@ -0,0 +1,17 @@
+import { Button } from './Button';
+
+export default {
+  title: 'Components/Atoms/Button',
+  component: Button,
+};
+
+const Template = (args) => {
+  return <Button {...args}>Read more</Button>;
+};
+
+export const Default = Template.bind({});
+
+export const Big = Template.bind({});
+Big.args = {
+  isBig: true,
+};
